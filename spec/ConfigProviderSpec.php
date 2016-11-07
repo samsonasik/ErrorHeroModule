06c4cb564d16a89b6d87682abf389d56cbf7caf5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-11-07 14:43:39 +0700

    spec style

--- spec/ConfigProviderSpec.php
+++ spec/ConfigProviderSpec.php
@@ -5,19 +5,25 @@ namespace ErrorHeroModule\Spec;
 use ErrorHeroModule\ConfigProvider;
 
 describe('ConfigProvider', function () {
+
     beforeAll(function () {
         $this->configProvider = new ConfigProvider();
     });
- 
+
     describe('->__invoke', function () {
+
         it('return "config" array with "dependencies" key', function () {
-            $moduleConfig = include __DIR__ . '/../config/module.config.php';
+
+            $moduleConfig = include __DIR__.'/../config/module.config.php';
             $expected = [
                 'dependencies' => $moduleConfig['service_manager'],
             ];
-            
+
             $actual = $this->configProvider->__invoke();
             expect($actual)->toBe($expected);
+
         });
+
     });
+
 });
--- spec/ModuleSpec.php
+++ spec/ModuleSpec.php
@@ -5,16 +5,22 @@ namespace ErrorHeroModule\Spec;
 use ErrorHeroModule\Module;
 
 describe('Module', function () {
+
     beforeAll(function () {
         $this->module = new Module();
     });
- 
+
     describe('->getConfig', function () {
+
         it('return "config" array', function () {
-            $moduleConfig = include __DIR__ . '/../config/module.config.php';
-            
+
+            $moduleConfig = include __DIR__.'/../config/module.config.php';
+
             $actual = $this->module->getConfig();
             expect($actual)->toBe($moduleConfig);
+
         });
+
     });
+
 });
