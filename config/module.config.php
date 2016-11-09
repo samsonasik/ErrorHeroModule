b6356ca69886ff1ab18a13da047b80510cdaa085
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-11-09 17:13:25 +0700

    error hero module config default value and view default for error

--- config/module.config.php
+++ config/module.config.php
@@ -3,9 +3,17 @@
 namespace ErrorHeroModule;
 
 return [
+
     'service_manager' => [
         'factories' => [
             Listner\Mvc::class => Listener\MvcFactory::class,
         ],
     ],
+
+    'view_manager' => [
+        'template_map' => [
+           'error-hero-module/error-default' => __DIR__.'/../view/error-hero-module/error-default.phtml',
+       ],
+    ],
+
 ];
--- src/Listener/MvcFactory.php
+++ src/Listener/MvcFactory.php
@@ -6,8 +6,31 @@ class MvcFactory
 {
     public function __invoke($container)
     {
-        return new Mvc(
-            $container->get('config')['error-hero-module']
-        );
+        $config = $container->get('config');
+        $errorHeroModuleConfig = isset($config['error-hero-module'])
+            ? $config['error-hero-module']
+            : [
+                'enable' => true,
+                'options' => [
+                    'exclude-php-errors' => [],
+                    'display_errors'  => 0,
+                    'view_errors' => 'error-hero-module/error-default'
+                ],
+                'logging' => [
+                    'range-same-error' => 86400,
+                    'adapters' => [
+                        'stream' => [
+                            'path' => '/var/log'
+                        ],
+                        'db' => [
+                            'zend-db-adapter' => 'Zend\Db\Adapter\Adapter',
+                            'table'           => 'log'
+                        ],
+                    ],
+                ],
+                'email-notification' => [],
+            ];
+
+        return new Mvc($errorHeroModuleConfig);
     }
 }
