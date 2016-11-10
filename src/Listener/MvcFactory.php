19ad89d5455b4291f81bc8d3acaa20def1d427e3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-11-10 10:05:36 +0700

    loggin setup

--- src/Listener/MvcFactory.php
+++ src/Listener/MvcFactory.php
@@ -8,35 +8,33 @@ class MvcFactory
     {
         $config = $container->get('config');
         $defaultHeroConfig = [
-            'error-hero-module' => [
-                'enable' => true,
-                'display-settings' => [
+            'enable' => true,
+            'display-settings' => [
 
-                    // excluded php errors
-                    'exclude-php-errors' => [
-                        E_USER_DEPRECATED
-                    ],
-
-                    // show or not error
-                    'display_errors'  => 1,
-
-                    // if enable and display_errors = 0
-                    'view_errors' => 'error-hero-module/error-default'
+                // excluded php errors
+                'exclude-php-errors' => [
+                    E_USER_DEPRECATED
                 ],
-                'logging-settings' => [
-                    'same-error' => 86400,
-                ],
-                'email-notification-settings' => [
-                    // set to true to activate email notification on log error
-                    'enable' => false,
 
-                    'mail-service'   => 'YourMailService', // Zend\Mail\Message instance registered at service manager
-                    'mail-transport' => 'YourMailTransport', // Zend\Mail\Transport\TransportInterface instance registered at service manager
+                // show or not error
+                'display_errors'  => 1,
 
-                    'email-to-send' => [
-                        'developer1@foo.com',
-                        'developer2@foo.com',
-                    ],
+                // if enable and display_errors = 0
+                'view_errors' => 'error-hero-module/error-default'
+            ],
+            'logging-settings' => [
+                'same-error' => 86400,
+            ],
+            'email-notification-settings' => [
+                // set to true to activate email notification on log error
+                'enable' => false,
+
+                'mail-service'   => 'YourMailService',   // Zend\Mail\Message instance registered at service manager
+                'mail-transport' => 'YourMailTransport', // Zend\Mail\Transport\TransportInterface instance registered at service manager
+
+                'email-to-send' => [
+                    'developer1@foo.com',
+                    'developer2@foo.com',
                 ],
             ],
         ];
