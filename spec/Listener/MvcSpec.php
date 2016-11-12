a59abf704d1d10eeda998f96d128ccdd85deed7e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-11-12 10:45:44 +0700

    more spec for handleException()

--- spec/Listener/MvcSpec.php
+++ spec/Listener/MvcSpec.php
@@ -129,7 +129,80 @@ describe('Mvc', function () {
 
         });
 
-        it('call logger->handleException() if $e->getParam("exception")', function () {
+        it('call logger->handleException() if $e->getParam("exception") and display_errors = 1', function () {
+
+            $config = [
+                'enable' => true,
+                'display-settings' => [
+
+                    // excluded php errors
+                    'exclude-php-errors' => [
+                        E_USER_DEPRECATED
+                    ],
+
+                    // show or not error
+                    'display_errors'  => 1,
+
+                    // if enable and display_errors = 0, the page will bring layout and view
+                    'template' => [
+                        'layout' => 'layout/layout',
+                        'view'   => 'error-hero-module/error-default'
+                    ],
+
+                    // if enable and display_errors = 0, the console will bring message
+                    'console' => [
+                        'message' => 'We have encountered a problem and we can not fulfill your request. An error report has been generated and send to the support team and someone will attend to this problem urgently. Please try again later. Thank you for your patience.',
+                    ],
+
+                ],
+                'logging-settings' => [
+                    'same-error-log-time-range' => 86400,
+                ],
+                'email-notification-settings' => [
+                    // set to true to activate email notification on log error
+                    'enable' => false,
+
+                    // Zend\Mail\Message instance registered at service manager
+                    'mail-message'   => 'YourMailMessageService',
+
+                    // Zend\Mail\Transport\TransportInterface instance registered at service manager
+                    'mail-transport' => 'YourMailTransportService',
+
+                    // email sender
+                    'email-from'    => 'Sender Name <sender@host.com>',
+
+                    'email-to-send' => [
+                        'developer1@foo.com',
+                        'developer2@foo.com',
+                    ],
+                ],
+            ];
+
+            $logging = Double::instance([
+                'extends' => Logging::class,
+                'methods' => '__construct'
+            ]);
+
+            $renderer = Double::instance(['extends' => PhpRenderer::class, 'methods' => '__construct']);
+
+            $listener =  new Mvc(
+                $config,
+                $logging,
+                $renderer
+            );
+
+            $exception = new \Exception();
+
+            $mvcEvent = Double::instance(['extends' => MvcEvent::class, 'methods' => '__construct']);
+            allow($mvcEvent)->toReceive('getParam')->andReturn($exception);
+            allow($logging)->toReceive('handleException')->with($exception);
+
+            $actual = $listener->exceptionError($mvcEvent);
+            expect($actual)->toBeNull(); // void
+            
+        });
+
+        it('call logger->handleException() with default console error message if $e->getParam("exception") and display_errors = 0', function () {
 
             Quit::disable();
             $exception = new \Exception();
@@ -144,9 +217,10 @@ describe('Mvc', function () {
             };
             expect($closure)->toThrow(new QuitException());
             ob_get_clean();
-            
+
         });
 
     });
+
 
 });
