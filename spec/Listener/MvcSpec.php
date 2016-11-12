3fe51a4a68a1c6f65c184b6581771229165d952a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-11-12 14:58:02 +0700

    spec for Mvc::phpError() with display_errors = 0 and 1

--- spec/Listener/MvcSpec.php
+++ spec/Listener/MvcSpec.php
@@ -243,4 +243,88 @@ describe('Mvc', function () {
 
     });
 
+    describe('->phpError()', function () {
+
+        it('set error_reporting & ini_set(display_errors) when display_errors config = 0', function () {
+
+                expect('error_reporting')->toBeCalled();
+                expect('ini_set')->toBeCalled();
+
+                $mvcEvent = Double::instance(['extends' => MvcEvent::class, 'methods' => '__construct']);
+                $this->listener->phpError($mvcEvent);
+
+        });
+
+        it('not set error_reporting & ini_set(display_errors) when display_errors config = 0', function () {
+
+                $config = [
+                    'enable' => true,
+                    'display-settings' => [
+
+                        // excluded php errors
+                        'exclude-php-errors' => [
+                            E_USER_DEPRECATED
+                        ],
+
+                        // show or not error
+                        'display_errors'  => 1,
+
+                        // if enable and display_errors = 0, the page will bring layout and view
+                        'template' => [
+                            'layout' => 'layout/layout',
+                            'view'   => 'error-hero-module/error-default'
+                        ],
+
+                        // if enable and display_errors = 0, the console will bring message
+                        'console' => [
+                            'message' => 'We have encountered a problem and we can not fulfill your request. An error report has been generated and send to the support team and someone will attend to this problem urgently. Please try again later. Thank you for your patience.',
+                        ],
+
+                    ],
+                    'logging-settings' => [
+                        'same-error-log-time-range' => 86400,
+                    ],
+                    'email-notification-settings' => [
+                        // set to true to activate email notification on log error
+                        'enable' => false,
+
+                        // Zend\Mail\Message instance registered at service manager
+                        'mail-message'   => 'YourMailMessageService',
+
+                        // Zend\Mail\Transport\TransportInterface instance registered at service manager
+                        'mail-transport' => 'YourMailTransportService',
+
+                        // email sender
+                        'email-from'    => 'Sender Name <sender@host.com>',
+
+                        'email-to-send' => [
+                            'developer1@foo.com',
+                            'developer2@foo.com',
+                        ],
+                    ],
+                ];
+
+                $logging = Double::instance([
+                    'extends' => Logging::class,
+                    'methods' => '__construct'
+                ]);
+
+                $renderer = Double::instance(['extends' => PhpRenderer::class, 'methods' => '__construct']);
+
+                $listener =  new Mvc(
+                    $config,
+                    $logging,
+                    $renderer
+                );
+
+                expect('error_reporting')->not->toBeCalled();
+                expect('ini_set')->not->toBeCalled();
+
+                $mvcEvent = Double::instance(['extends' => MvcEvent::class, 'methods' => '__construct']);
+                $listener->phpError($mvcEvent);
+
+        });
+
+    });
+
 });
