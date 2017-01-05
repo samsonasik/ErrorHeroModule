169043482f4e14bdedb542ecf5813236759a4c94
Abdul Malik Ikhsan <samsonasik@gmail.com>
2017-01-05 13:08:40 +0700

    command "php"

--- src/Handler/LoggingFactory.php
+++ src/Handler/LoggingFactory.php
@@ -19,7 +19,7 @@ class LoggingFactory
         } else {
             $serverUrl  = php_uname('n');
             $request    = new ConsoleRequest();
-            $requestUri = '$ ' . $request->getScriptName() . ' ' . $request->toString();
+            $requestUri = '$ php ' . $request->getScriptName() . ' ' . $request->toString();
         }
 
         $config                = $container->get('config');
