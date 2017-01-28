ec7996ff9734ca8026310f76eaee10add040e256
Abdul Malik Ikhsan <samsonasik@gmail.com>
2017-01-28 18:02:35 +0700

    test for cannot connect to db

---
+++ spec/Fixture/autoload-for-cannot-connect-to-db/error-hero-module.local.php
@@ -0,0 +1,101 @@
+<?php
+
+use Zend\Mail\Message;
+use Zend\Mail\Transport\InMemory;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+
+    'service_manager' => [
+        'factories' => [
+            Message::class => InvokableFactory::class,
+            InMemory::class    => InvokableFactory::class,
+        ],
+    ],
+
+    'db' => [
+        'username' => 'root',
+        'password' => 'wrong_password',
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=errorheromodule;host=127.0.0.1',
+        'driver_options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
+        ],
+    ],
+
+    'log' => [
+        'ErrorHeroModuleLogger' => [
+            'writers' => [
+
+                [
+                    'name' => 'db',
+                    'options' => [
+                        'db'     => 'Zend\Db\Adapter\Adapter',
+                        'table'  => 'log',
+                        'column' => [
+                            'timestamp' => 'date',
+                            'priority'  => 'type',
+                            'message'   => 'event',
+                            'extra'     => [
+                                'url'  => 'url',
+                                'file' => 'file',
+                                'line' => 'line',
+                                'error_type' => 'error_type',
+                                'trace'      => 'trace',
+                                'request_data' => 'request_data',
+                            ],
+                        ],
+                    ],
+                ],
+
+            ],
+        ],
+    ],
+
+    'error-hero-module' => [
+        'enable' => true,
+        'display-settings' => [
+
+            // excluded php errors
+            'exclude-php-errors' => [
+                E_USER_DEPRECATED
+            ],
+
+            // show or not error
+            'display_errors'  => 0,
+
+            // if enable and display_errors = 0, the page will bring layout and view
+            'template' => [
+                'layout' => 'layout/layout',
+                'view'   => 'error-hero-module/error-default'
+            ],
+
+            // if enable and display_errors = 0, the console will bring message
+            'console' => [
+                'message' => 'We have encountered a problem and we can not fulfill your request. An error report has been generated and send to the support team and someone will attend to this problem urgently. Please try again later. Thank you for your patience.',
+            ],
+
+        ],
+        'logging-settings' => [
+            'same-error-log-time-range' => 86400,
+        ],
+        'email-notification-settings' => [
+            // set to true to activate email notification on log error
+            'enable' => true,
+
+            // Zend\Mail\Message instance registered at service manager
+            'mail-message'   => Message::class,
+
+            // Zend\Mail\Transport\TransportInterface instance registered at service manager
+            'mail-transport' => InMemory::class,
+
+            // email sender
+            'email-from'    => 'Sender Name <sender@host.com>',
+
+            'email-to-send' => [
+                'developer1@foo.com',
+                'developer2@foo.com',
+            ],
+        ],
+    ],
+];
---
+++ spec/IntegrationViaErrorPreviewControllerForCannotConnecttoDBSpec.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace ErrorHeroModule\Spec;
+
+use ErrorHeroModule;
+use ErrorHeroModule\Controller\ErrorPreviewController;
+use Kahlan\Plugin\Quit;
+use Kahlan\QuitException;
+use Zend\Console\Console;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Zend\Log;
+use Zend\Mvc\Application;
+
+describe('Integration via ErrorPreviewController for Cannot connect to DB', function () {
+
+    given('application', function () {
+
+        Console::overrideIsConsole(false);
+
+        $application = Application::init([
+            'modules' => [
+                'Zend\Router',
+                'Zend\Db',
+                'ErrorHeroModule',
+            ],
+            'module_listener_options' => [
+                'config_glob_paths' => [
+                    realpath(__DIR__).'/Fixture/autoload-for-cannot-connect-to-db/{{,*.}global,{,*.}local}.php',
+                ],
+            ],
+        ]);
+
+        $events         = $application->getEventManager();
+        $serviceManager = $application->getServiceManager();
+        $serviceManager->get('SendResponseListener')
+                       ->detach($events);
+
+        return $application;
+
+    });
+
+    describe('/error-preview', function() {
+
+        it('show error page', function() {
+
+            Quit::disable();
+
+            $request     = $this->application->getRequest();
+            $request->setMethod('GET');
+            $request->setUri('/error-preview');
+
+            ob_start();
+            $closure = function () {
+                $this->application->run();
+            };
+            expect($closure)->toThrow(new QuitException('Exit statement occurred', -1));
+            $content = ob_get_clean();
+
+            expect($content)->toContain('<p>We have encountered a problem and we can not fulfill your request');
+
+        });
+
+    });
+
+    describe('/error-preview/error', function() {
+
+        it('show error page', function() {
+
+            Quit::disable();
+
+            $request     = $this->application->getRequest();
+            $request->setMethod('GET');
+            $request->setUri('/error-preview/error');
+
+            ob_start();
+            $closure = function () {
+                $this->application->run();
+            };
+            expect($closure)->toThrow(new QuitException('Exit statement occurred', -1));
+            $content = ob_get_clean();
+
+            expect($content)->toContain('<p>We have encountered a problem and we can not fulfill your request');
+
+        });
+    });
+
+});
