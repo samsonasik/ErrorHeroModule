772356d4911b042ad7942402ac0771f00f1ee8e7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2017-02-26 17:20:50 +0700

    move shared property to trait

--- src/HeroTrait.php
+++ src/HeroTrait.php
@@ -5,6 +5,39 @@ namespace ErrorHeroModule;
 trait HeroTrait
 {
     /**
+     * @var array
+     */
+    private $errorHeroModuleConfig;
+
+    /**
+     * @var Logging
+     */
+    private $logging;
+
+    /**
+     * @var PhpRenderer
+     */
+    private $renderer;
+
+    private $errorType = [
+        E_ERROR             => 'E_ERROR',
+        E_WARNING           => 'E_WARNING',
+        E_PARSE             => 'E_PARSE',
+        E_NOTICE            => 'E_NOTICE',
+        E_CORE_ERROR        => 'E_CORE_ERROR',
+        E_CORE_WARNING      => 'E_CORE_WARNING',
+        E_COMPILE_ERROR     => 'E_COMPILE_ERROR',
+        E_COMPILE_WARNING   => 'E_COMPILE_WARNING',
+        E_USER_ERROR        => 'E_USER_ERROR',
+        E_USER_WARNING      => 'E_USER_WARNING',
+        E_USER_NOTICE       => 'E_USER_NOTICE',
+        E_STRICT            => 'E_STRICT',
+        E_RECOVERABLE_ERROR => 'E_RECOVERABLE_ERROR',
+        E_DEPRECATED        => 'E_DEPRECATED',
+        E_USER_DEPRECATED   => 'E_USER_DEPRECATED',
+    ];
+
+    /**
      * @return void
      */
     public function execOnShutdown()
