9476e1e73a568bf99088b3452f25d81bdf8c3229
Abdul Malik Ikhsan <samsonasik@gmail.com>
2017-02-28 17:59:48 +0700

    expressive middleware template test after dependencies applied

--- spec/Middleware/ExpressiveSpec.php
+++ spec/Middleware/ExpressiveSpec.php
@@ -3,15 +3,89 @@
 namespace ErrorHeroModule\Spec\Middleware;
 
 use ErrorHeroModule\Middleware\Expressive;
-use Kahlan\Plugin\Double;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response;
+use ErrorHeroModule\Handler\Logging;
+use Kahlan\Plugin\Double;
+use Kahlan\Plugin\Quit;
+use Kahlan\QuitException;
+use Zend\Console\Console;
+use Zend\Db\Adapter\Adapter;
+use Zend\Http\PhpEnvironment\Request;
+use Zend\Log\Logger;
+use Zend\Log\Writer\Db as DbWriter;
+use Zend\View\Renderer\PhpRenderer;
+use Zend\View\Resolver;
 
 describe('Expressive', function () {
 
+    given('logging', function () {
+        return Double::instance([
+            'extends' => Logging::class,
+            'methods' => '__construct'
+        ]);
+    });
+
+    given('renderer', function () {
+        return Double::instance(['extends' => PhpRenderer::class, 'methods' => '__construct']);
+    });
+
+    given('config', function () {
+        return [
+            'enable' => true,
+            'display-settings' => [
+
+                // excluded php errors
+                'exclude-php-errors' => [
+                    E_USER_DEPRECATED
+                ],
+
+                // show or not error
+                'display_errors'  => 0,
+
+                // if enable and display_errors = 0, the page will bring layout and view
+                'template' => [
+                    'layout' => 'layout/layout',
+                    'view'   => 'error-hero-module/error-default'
+                ],
+
+                // if enable and display_errors = 0, the console will bring message
+                'console' => [
+                    'message' => 'We have encountered a problem and we can not fulfill your request. An error report has been generated and send to the support team and someone will attend to this problem urgently. Please try again later. Thank you for your patience.',
+                ],
+
+            ],
+            'logging-settings' => [
+                'same-error-log-time-range' => 86400,
+            ],
+            'email-notification-settings' => [
+                // set to true to activate email notification on log error
+                'enable' => false,
+
+                // Zend\Mail\Message instance registered at service manager
+                'mail-message'   => 'YourMailMessageService',
+
+                // Zend\Mail\Transport\TransportInterface instance registered at service manager
+                'mail-transport' => 'YourMailTransportService',
+
+                // email sender
+                'email-from'    => 'Sender Name <sender@host.com>',
+
+                'email-to-send' => [
+                    'developer1@foo.com',
+                    'developer2@foo.com',
+                ],
+            ],
+        ];
+    });
+
     given('middleware', function () {
-        return new Expressive();
+        return new Expressive(
+            $this->config,
+            $this->logging,
+            $this->renderer
+        );
     });
 
     describe('->__invoke()', function () {
