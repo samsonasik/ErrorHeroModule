72af7997224376b8506b0f2fcbb869340946d62a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2017-02-28 17:58:04 +0700

    Expressive Factory and config setting

--- src/Middleware/Expressive.php
+++ src/Middleware/Expressive.php
@@ -2,17 +2,52 @@
 
 namespace ErrorHeroModule\Middleware;
 
+use ErrorHeroModule\Handler\Logging;
+use ErrorHeroModule\HeroTrait;
+use Exception;
+use Seld\JsonLint\JsonParser;
+use Throwable;
+use Zend\Console\Console;
+use Zend\Console\Response as ConsoleResponse;
+use Zend\EventManager\AbstractListenerAggregate;
+use Zend\EventManager\Event;
+use Zend\EventManager\EventManagerInterface;
+use Zend\Http\PhpEnvironment\Request;
+use Zend\Http\PhpEnvironment\Response as HttpResponse;
+use Zend\Mvc\MvcEvent;
+use Zend\Text\Table;
+use Zend\View\Model\ViewModel;
+use Zend\View\Renderer\PhpRenderer;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class Expressive
 {
+    use HeroTrait;
+
+    /**
+     * @param array       $errorHeroModuleConfig
+     * @param Logging     $logging
+     * @param PhpRenderer $renderer
+     */
+    public function __construct(
+        array       $errorHeroModuleConfig,
+        Logging     $logging,
+        PhpRenderer $renderer
+    ) {
+        $this->errorHeroModuleConfig = $errorHeroModuleConfig;
+        $this->logging               = $logging;
+        $this->renderer              = $renderer;
+    }
+
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
         try {
             return $next($request, $response);
-        } catch (\Throwable $t) {
-        } catch (\Exception $e) {
+        } catch (Throwable $t) {
+
+        } catch (Exception $e) {
+            
         }
     }
 }
---
+++ src/Middleware/ExpressiveFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ErrorHeroModule\Listener;
+
+use ErrorHeroModule\Handler\Logging;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class ExpresiveFactory
+{
+    /**
+     * @param ContainerInterface|ServiceLocatorInterface
+     *
+     * @return Mvc
+     */
+    public function __invoke($container)
+    {
+        $config = $container->get('config');
+
+        return new Mvc(
+            $config['error-hero-module'],
+            $container->get(Logging::class),
+            $container->get('ViewRenderer')
+        );
+    }
+}
