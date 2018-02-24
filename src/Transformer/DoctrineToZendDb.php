9be58541f15aa39492728835f46f55857d6667f6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-02-25 02:45:44 +0700

    space

--- src/Transformer/DoctrineToZendDb.php
+++ src/Transformer/DoctrineToZendDb.php
@@ -28,7 +28,6 @@ class DoctrineToZendDb
             'driver_options' => $driverOptions,
         ];
 
-
         $allowOverride = $container->getAllowOverride();
         $container->setAllowOverride(true);
 
