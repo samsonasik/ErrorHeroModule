2b5898523d03604b2b1f1655a954d912202d9dc3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-04-15 04:58:50 +0700

    typo

--- spec/Handler/LoggingFactorySpec.php
+++ spec/Handler/LoggingFactorySpec.php
@@ -119,7 +119,7 @@ describe('LoggingFactorySpec', function () {
 
         });
 
-        it('instance of Logging on non-console with container does has "Request" service', function () {
+        it('instance of Logging on non-console with container has "Request" service', function () {
 
             Console::overrideIsConsole(false);
             $config = [
