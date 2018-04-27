fe9c1a1d7e87178d7a391c7449f2dec18bd58e4d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-04-27 21:16:35 +0700

    html

--- spec/Listener/MvcSpec.php
+++ spec/Listener/MvcSpec.php
@@ -264,7 +264,7 @@ describe('Mvc', function () {
             expect($closure)->toThrow(new QuitException('Exit statement occurred', -1));
             $content = ob_get_clean();
 
-            expect($content)->toContain('We have encountered a problem');
+            expect($content)->toContain('<p>We have encountered a problem');
 
         });
 
