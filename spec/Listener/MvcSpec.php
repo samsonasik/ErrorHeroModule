8a456c00debbb3a833f20232a8066cd55a6b28e8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-05-28 04:29:03 +0700

    reduce repetitive setup at Mvc listener tests

--- spec/Listener/MvcSpec.php
+++ spec/Listener/MvcSpec.php
@@ -140,52 +140,8 @@ describe('Mvc', function () {
 
         it('call logging->handleErrorException() if $e->getParam("exception") and display_errors = 1', function () {
 
-            $config = [
-                'enable' => true,
-                'display-settings' => [
-
-                    // excluded php errors
-                    'exclude-php-errors' => [
-                        \E_USER_DEPRECATED
-                    ],
-
-                    // show or not error
-                    'display_errors'  => 1,
-
-                    // if enable and display_errors = 0, the page will bring layout and view
-                    'template' => [
-                        'layout' => 'layout/layout',
-                        'view'   => 'error-hero-module/error-default'
-                    ],
-
-                    // if enable and display_errors = 0, the console will bring message
-                    'console' => [
-                        'message' => 'We have encountered a problem and we can not fulfill your request. An error report has been generated and sent to the support team and someone will attend to this problem urgently. Please try again later. Thank you for your patience.',
-                    ],
-
-                ],
-                'logging-settings' => [
-                    'same-error-log-time-range' => 86400,
-                ],
-                'email-notification-settings' => [
-                    // set to true to activate email notification on log error
-                    'enable' => false,
-
-                    // Zend\Mail\Message instance registered at service manager
-                    'mail-message'   => 'YourMailMessageService',
-
-                    // Zend\Mail\Transport\TransportInterface instance registered at service manager
-                    'mail-transport' => 'YourMailTransportService',
-
-                    // email sender
-                    'email-from'    => 'Sender Name <sender@host.com>',
-
-                    'email-to-send' => [
-                        'developer1@foo.com',
-                        'developer2@foo.com',
-                    ],
-                ],
-            ];
+            $config = $this->config;
+            $config['display-settings']['display_errors'] = 1;
 
             $logging = Double::instance([
                 'extends' => Logging::class,
@@ -257,57 +213,8 @@ describe('Mvc', function () {
 
         it('do not call logging->handleErrorException() if $e->getParam("exception") and has excluded exception match', function () {
 
-            $config = [
-                'enable' => true,
-                'display-settings' => [
-
-                    // excluded php errors
-                    'exclude-php-errors' => [
-                        \E_USER_DEPRECATED
-                    ],
-
-                    // excluded exceptions
-                    'exclude-exceptions' => [
-                        \Exception::class, // can be an Exception class or class extends Exception class
-                    ],
-
-                    // show or not error
-                    'display_errors'  => 1,
-
-                    // if enable and display_errors = 0, the page will bring layout and view
-                    'template' => [
-                        'layout' => 'layout/layout',
-                        'view'   => 'error-hero-module/error-default'
-                    ],
-
-                    // if enable and display_errors = 0, the console will bring message
-                    'console' => [
-                        'message' => 'We have encountered a problem and we can not fulfill your request. An error report has been generated and sent to the support team and someone will attend to this problem urgently. Please try again later. Thank you for your patience.',
-                    ],
-
-                ],
-                'logging-settings' => [
-                    'same-error-log-time-range' => 86400,
-                ],
-                'email-notification-settings' => [
-                    // set to true to activate email notification on log error
-                    'enable' => false,
-
-                    // Zend\Mail\Message instance registered at service manager
-                    'mail-message'   => 'YourMailMessageService',
-
-                    // Zend\Mail\Transport\TransportInterface instance registered at service manager
-                    'mail-transport' => 'YourMailTransportService',
-
-                    // email sender
-                    'email-from'    => 'Sender Name <sender@host.com>',
-
-                    'email-to-send' => [
-                        'developer1@foo.com',
-                        'developer2@foo.com',
-                    ],
-                ],
-            ];
+            $config = $this->config;
+            $config['display-settings']['exclude-exceptions'] = [\Exception::class];
 
             $logging = Double::instance([
                 'extends' => Logging::class,
