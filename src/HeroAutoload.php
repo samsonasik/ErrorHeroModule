98d28ca33c920fb4842fc7017f5a18e544ea5cff
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-09-17 13:17:37 +0700

    handle trait_exists in HeroAutoload

--- src/HeroAutoload.php
+++ src/HeroAutoload.php
@@ -8,26 +8,24 @@ use RuntimeException;
 
 class HeroAutoload
 {
-    public static function handle(string $class)
+    public static function handle(string $classOrTraitOrInterface)
     {
-        if (\class_exists($class, false)) {
-            return;
-        }
-
         $debugBacktrace = \debug_backtrace();
         if (
             isset($debugBacktrace[1]['function'], $debugBacktrace[2]['function']) &&
             $debugBacktrace[1]['function'] === 'spl_autoload_call' &&
             (
-                $debugBacktrace[2]['function'] === 'class_exists' || $debugBacktrace[2]['function'] === 'interface_exists'
+                $debugBacktrace[2]['function'] === 'class_exists' ||
+                $debugBacktrace[2]['function'] === 'trait_exists' ||
+                $debugBacktrace[2]['function'] === 'interface_exists'
             )
         ) {
             return;
         }
 
         throw new RuntimeException(sprintf(
-            'class %s not found',
-            $class
+            'class or trait or interface %s not found',
+            $classOrTraitOrInterface
         ));
     }
 }
