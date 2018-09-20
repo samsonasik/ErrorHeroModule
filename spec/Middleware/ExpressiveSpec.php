a5b661c1e8e5932fc57315c0c96477ee8f10d992
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-09-21 02:36:36 +0700

    test exec shutdown on middleware

--- spec/Middleware/ExpressiveSpec.php
+++ spec/Middleware/ExpressiveSpec.php
@@ -2,6 +2,7 @@
 
 namespace ErrorHeroModule\Spec\Middleware;
 
+use Closure;
 use ErrorHeroModule\Handler\Logging;
 use ErrorHeroModule\Middleware\Expressive;
 use Kahlan\Plugin\Double;
@@ -523,6 +524,291 @@ json
             expect($logging)->not->toReceive('handleErrorException');
 
         });
+
+    });
+
+    describe('->execOnShutdown()', function ()  {
+
+        it('call error_get_last() and return nothing', function () {
+
+            allow('error_get_last')->toBeCalled()->andReturn(null);
+            expect($this->middleware->execOnShutdown())->toBeNull();
+
+        });
+
+        it('call error_get_last() and property_exists() after null check passed and throws', function () {
+
+            allow('error_get_last')->toBeCalled()->andReturn([
+                'type' => 3,
+                'message' => 'class@anonymous cannot implement stdClass - it is not an interface',
+                'file' => '/var/www/zf/templates/app/home-page.phtml',
+                'line' => 2
+            ]);
+
+            $dbAdapter = new Adapter([
+                'username' => 'root',
+                'password' => '',
+                'driver' => 'Pdo',
+                'dsn' => 'mysql:dbname=errorheromodule;host=127.0.0.1',
+                'driver_options' => [
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
+                ],
+            ]);
+
+            $writer = new DbWriter(
+                [
+                    'db' => $dbAdapter,
+                    'table' => 'log',
+                    'column' => [
+                        'timestamp' => 'date',
+                        'priority'  => 'type',
+                        'message'   => 'event',
+                        'extra'     => [
+                            'url'  => 'url',
+                            'file' => 'file',
+                            'line' => 'line',
+                            'error_type' => 'error_type',
+                            'trace'      => 'trace',
+                            'request_data' => 'request_data',
+                        ],
+                    ],
+                ]
+            );
+
+            $logger = new Logger();
+            $logger->addWriter($writer);
+            $logWritersConfig = [
+
+                [
+                    'name' => 'db',
+                    'options' => [
+                        'db'     => Adapter::class,
+                        'table'  => 'log',
+                        'column' => [
+                            'timestamp' => 'date',
+                            'priority'  => 'type',
+                            'message'   => 'event',
+                            'extra'     => [
+                                'url'  => 'url',
+                                'file' => 'file',
+                                'line' => 'line',
+                                'error_type' => 'error_type',
+                                'trace'      => 'trace',
+                                'request_data' => 'request_data',
+                            ],
+                        ],
+                    ],
+                ],
+
+            ];
+
+            $logging = new Logging(
+                $logger,
+                $this->config,
+                $logWritersConfig,
+                null,
+                null
+            );
+
+            $errorHeroModuleLocalConfig  = [
+                'enable' => true,
+                'display-settings' => [
+                    'exclude-php-errors' => [
+                        \E_USER_DEPRECATED
+                    ],
+                    'display_errors'  => 1,
+                    'template' => [
+                        'layout' => 'layout/layout',
+                        'view'   => 'error-hero-module/error-default'
+                    ],
+                    'console' => [
+                        'message' => 'We have encountered a problem and we can not fulfill your request. An error report has been generated and sent to the support team and someone will attend to this problem urgently. Please try again later. Thank you for your patience.',
+                    ],
+
+                ],
+                'logging-settings' => [
+                    'same-error-log-time-range' => 86400,
+                ],
+                'email-notification-settings' => [
+                    'enable' => false,
+                    'mail-message'   => 'YourMailMessageService',
+                    'mail-transport' => 'YourMailTransportService',
+                    'email-from'    => 'Sender Name <sender@host.com>',
+                    'email-to-send' => [
+                        'developer1@foo.com',
+                        'developer2@foo.com',
+                    ],
+                ],
+            ];
+
+            $middleware = new Expressive(
+                $errorHeroModuleLocalConfig,
+                $logging,
+                $this->renderer
+            );
+
+            allow('property_exists')->toBeCalled()->with($middleware, 'request')->andReturn(true);
+            allow('property_exists')->toBeCalled()->with($middleware, 'mvcEvent')->andReturn(false);
+
+            $request = & Closure::bind(function & ($middleware) {
+                return $middleware->request;
+            }, null, $middleware)($middleware);
+            $request = new ServerRequest(
+                [],
+                [],
+                new Uri('http://example.com'),
+                'GET',
+                'php://memory',
+                [],
+                [],
+                [],
+                '',
+                '1.2'
+            );
+
+            $closure = function () use ($middleware) {
+                $middleware->execOnShutdown();
+            };
+            expect($closure)->toThrow(new \ErrorException(
+                'class@anonymous cannot implement stdClass - it is not an interface'
+            ));
+
+        });
+
+        it('call error_get_last() and property_exists() after null check passed', function () {
+
+            allow('error_get_last')->toBeCalled()->andReturn([
+                'type' => 3,
+                'message' => 'class@anonymous cannot implement stdClass - it is not an interface',
+                'file' => '/var/www/zf/templates/app/home-page.phtml',
+                'line' => 2
+            ]);
+
+            $dbAdapter = new Adapter([
+                'username' => 'root',
+                'password' => '',
+                'driver' => 'Pdo',
+                'dsn' => 'mysql:dbname=errorheromodule;host=127.0.0.1',
+                'driver_options' => [
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
+                ],
+            ]);
+
+            $writer = new DbWriter(
+                [
+                    'db' => $dbAdapter,
+                    'table' => 'log',
+                    'column' => [
+                        'timestamp' => 'date',
+                        'priority'  => 'type',
+                        'message'   => 'event',
+                        'extra'     => [
+                            'url'  => 'url',
+                            'file' => 'file',
+                            'line' => 'line',
+                            'error_type' => 'error_type',
+                            'trace'      => 'trace',
+                            'request_data' => 'request_data',
+                        ],
+                    ],
+                ]
+            );
+
+            $logger = new Logger();
+            $logger->addWriter($writer);
+            $logWritersConfig = [
+
+                [
+                    'name' => 'db',
+                    'options' => [
+                        'db'     => Adapter::class,
+                        'table'  => 'log',
+                        'column' => [
+                            'timestamp' => 'date',
+                            'priority'  => 'type',
+                            'message'   => 'event',
+                            'extra'     => [
+                                'url'  => 'url',
+                                'file' => 'file',
+                                'line' => 'line',
+                                'error_type' => 'error_type',
+                                'trace'      => 'trace',
+                                'request_data' => 'request_data',
+                            ],
+                        ],
+                    ],
+                ],
+
+            ];
+
+            $logging = new Logging(
+                $logger,
+                $this->config,
+                $logWritersConfig,
+                null,
+                null
+            );
+
+            $errorHeroModuleLocalConfig  = [
+                'enable' => true,
+                'display-settings' => [
+                    'exclude-php-errors' => [
+                        \E_USER_DEPRECATED
+                    ],
+                    'display_errors'  => 0,
+                    'template' => [
+                        'layout' => 'layout/layout',
+                        'view'   => 'error-hero-module/error-default'
+                    ],
+                    'console' => [
+                        'message' => 'We have encountered a problem and we can not fulfill your request. An error report has been generated and sent to the support team and someone will attend to this problem urgently. Please try again later. Thank you for your patience.',
+                    ],
+
+                ],
+                'logging-settings' => [
+                    'same-error-log-time-range' => 86400,
+                ],
+                'email-notification-settings' => [
+                    'enable' => false,
+                    'mail-message'   => 'YourMailMessageService',
+                    'mail-transport' => 'YourMailTransportService',
+                    'email-from'    => 'Sender Name <sender@host.com>',
+                    'email-to-send' => [
+                        'developer1@foo.com',
+                        'developer2@foo.com',
+                    ],
+                ],
+            ];
+
+            $middleware = new Expressive(
+                $errorHeroModuleLocalConfig,
+                $logging,
+                $this->renderer
+            );
+
+            allow('property_exists')->toBeCalled()->with($middleware, 'request')->andReturn(true);
+            allow('property_exists')->toBeCalled()->with($middleware, 'mvcEvent')->andReturn(false);
+
+            $request = & Closure::bind(function & ($middleware) {
+                return $middleware->request;
+            }, null, $middleware)($middleware);
+            $request = new ServerRequest(
+                [],
+                [],
+                new Uri('http://example.com'),
+                'GET',
+                'php://memory',
+                [],
+                [],
+                [],
+                '',
+                '1.2'
+            );
+
+            expect($middleware->execOnShutdown())->toBeNull();
+
+        });
+
 
     });
 
