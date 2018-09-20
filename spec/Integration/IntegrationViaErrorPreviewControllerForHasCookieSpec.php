d6b0473b6e686d2ed02df9b6adccb5444832b63d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2018-09-21 01:19:15 +0700

    Revert "Revert "Revert "more integration tests for fatal error"""
    This reverts commit f3d0028633df0e2951b95c115e3bc043ffae73b2.

--- spec/Integration/IntegrationViaErrorPreviewControllerForHasCookieSpec.php
+++ spec/Integration/IntegrationViaErrorPreviewControllerForHasCookieSpec.php
@@ -83,24 +83,4 @@ describe('Integration via ErrorPreviewController for has cookie', function () {
         });
     });
 
-    describe('/error-preview/fatal', function() {
-
-        it('show error page', function() {
-
-            $request     = $this->application->getRequest();
-            $request->setMethod('GET');
-            $request->setUri('http://example.com/error-preview/fatal');
-            $request->setRequestUri('/error-preview/fatal');
-
-            \ob_start();
-            $this->application->run();
-            $content = \ob_get_clean();
-
-            expect($content)->toContain('<title>Error');
-            expect($content)->toContain('<p>We have encountered a problem and we can not fulfill your request');
-            expect($this->application->getResponse()->getStatusCode())->toBe(500);
-
-        });
-    });
-
 });
