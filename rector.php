29922df13867dc5e31685d61679c185b1c61cd4c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-06-29 17:34:19 +0700

    update rector

--- rector.php
+++ rector.php
@@ -14,7 +14,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PATHS, [__DIR__ . '/config', __DIR__ . '/src', __DIR__ . '/spec']);
 
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-    $parameters->set(Option::ENABLE_CACHE, true);
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
 
     $parameters->set(Option::SKIP, [
