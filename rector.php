1da9c8a42775e045a5d202a518358f21d3e9454a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-07-03 23:00:33 +0700

    more setlist

--- rector.php
+++ rector.php
@@ -1,20 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\Php71\Rector\FuncCall\CountOnNullRector;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(SetList::CODING_STYLE);
     $containerConfigurator->import(SetList::CODE_QUALITY_STRICT);
+    $containerConfigurator->import(SetList::CODING_STYLE);
+    $containerConfigurator->import(SetList::ORDER);
+    $containerConfigurator->import(SetList::PHP_70);
+    $containerConfigurator->import(SetList::PHP_71);
+    $containerConfigurator->import(SetList::PHP_72);
+    $containerConfigurator->import(SetList::PHP_73);
     $containerConfigurator->import(SetList::PHP_74);
     $containerConfigurator->import(SetList::PHP_80);
-    $containerConfigurator->import(SetList::ORDER);
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [__DIR__ . '/config', __DIR__ . '/src', __DIR__ . '/spec']);
 
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
+
+    $parameters->set(Option::SKIP, [
+        CountOnNullRector::class,
+    ]);
 };
