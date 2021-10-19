b22e7c34879e4613a0bc47f25df555533cb99fc7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-10-19 22:09:03 +0700

    using rector LevelSetList

--- rector.php
+++ rector.php
@@ -6,11 +6,14 @@ use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
 use Rector\CodingStyle\Rector\ClassMethod\UnSpreadOperatorRector;
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\Php56\Rector\FunctionLike\AddDefaultValueForUndefinedVariableRector;
 use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Set\ValueObject\SetList;
+use Rector\Set\ValueObject\LevelSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
     $containerConfigurator->import(SetList::CODE_QUALITY);
     $containerConfigurator->import(SetList::CODING_STYLE);
     $containerConfigurator->import(SetList::CODING_STYLE_ADVANCED);
@@ -18,12 +21,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(SetList::DEFLUENT);
     $containerConfigurator->import(SetList::NAMING);
     $containerConfigurator->import(SetList::ORDER);
-    $containerConfigurator->import(SetList::PHP_70);
-    $containerConfigurator->import(SetList::PHP_71);
-    $containerConfigurator->import(SetList::PHP_72);
-    $containerConfigurator->import(SetList::PHP_73);
-    $containerConfigurator->import(SetList::PHP_74);
-    $containerConfigurator->import(SetList::PHP_80);
     $containerConfigurator->import(SetList::PRIVATIZATION);
     $containerConfigurator->import(SetList::PSR_4);
     $containerConfigurator->import(SetList::TYPE_DECLARATION);
@@ -41,5 +38,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         CallableThisArrayToAnonymousFunctionRector::class,
         UnSpreadOperatorRector::class,
         FinalizeClassesWithoutChildrenRector::class,
+        AddDefaultValueForUndefinedVariableRector::class,
     ]);
 };
