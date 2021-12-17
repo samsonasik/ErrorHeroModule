e6ae509fcb002492723d8a9ac37062a8b8efc4fa
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-17 19:22:01 +0700

    docblock on MvcFactory

--- src/Listener/MvcFactory.php
+++ src/Listener/MvcFactory.php
@@ -5,18 +5,24 @@ declare(strict_types=1);
 namespace ErrorHeroModule\Listener;
 
 use ErrorHeroModule\Handler\Logging;
+use Laminas\View\Renderer\PhpRenderer;
 use Psr\Container\ContainerInterface;
 
 class MvcFactory
 {
     public function __invoke(ContainerInterface $container): Mvc
     {
+        /** @var array $config */
         $config = $container->get('config');
+        /** @var Logging $logging */
+        $logging = $container->get(Logging::class);
+        /** @var PhpRenderer $viewRenderer */
+        $viewRenderer = $container->get('ViewRenderer');
 
         return new Mvc(
             $config['error-hero-module'],
-            $container->get(Logging::class),
-            $container->get('ViewRenderer')
+            $logging,
+            $viewRenderer
         );
     }
 }
