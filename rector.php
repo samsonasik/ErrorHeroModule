32c0c16c71da6a2fff7d769e2ccd692fab5fd84f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-29 02:06:33 +0700

    try parallel rector

--- rector.php
+++ rector.php
@@ -23,8 +23,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(SetList::TYPE_DECLARATION_STRICT);
 
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [__DIR__ . '/config', __DIR__ . '/src', __DIR__ . '/spec']);
-
+    $parameters->set(Option::PARALLEL, true);
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
 
     $parameters->set(Option::SKIP, [
