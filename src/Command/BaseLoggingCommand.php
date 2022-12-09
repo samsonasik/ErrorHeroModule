92685630578d72e0214fc540af1ca6b06f327cea
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-12-09 14:19:37 +0700

    prepare running override

--- src/Command/BaseLoggingCommand.php
+++ src/Command/BaseLoggingCommand.php
@@ -4,7 +4,39 @@ declare(strict_types=1);
 
 namespace ErrorHeroModule\Command;
 
-// to be extended in laminas-cli app
-abstract class BaseLoggingCommand
+use ErrorHeroModule\Handler\Logging;
+use Exception;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
+
+abstract class BaseLoggingCommand extends Command
 {
+    public function __construct(
+        private array $errorHeroModuleConfig,
+        private readonly Logging $logging
+    ) {
+    }
+
+    public function run(InputInterface $input, OutputInterface $output): int
+    {
+        static $isRun;
+
+        if ($isRun === true) {
+            return 0;
+        }
+
+        $isRun = true;
+        try {
+            return static::run($input, $output);
+        } catch (Throwable $throwable) {}
+
+        return $this->exceptionError($throwable);
+    }
+
+    private function exceptionError(Throwable $throwable): int
+    {
+        return 1;
+    }
 }
