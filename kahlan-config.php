5d62cc5b0d684ab94066cd28f8d92d2ff9c48025
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-12-11 08:57:02 +0700

    ignore preview page on kahlan config

--- kahlan-config.php
+++ kahlan-config.php
@@ -19,8 +19,13 @@ Filters::apply($this, 'coverage', function($next) {
         'driver'    => new Xdebug(),
         'path'      => $this->commandLine()->get('src'),
         'exclude'   => [
-            'src/Controller/ErrorPreviewConsoleController.php',
+            // laminas-cli preview console command
+            'src/Command/Preview/ErrorPreviewConsoleCommand.php',
+
+            // laminas-mvc preview page
             'src/Controller/ErrorPreviewController.php',
+
+            // mezzio preview page
             'src/Middleware/Routed/Preview/ErrorPreviewAction.php',
         ],
         'colors'    => ! $this->commandLine()->get('no-colors')
