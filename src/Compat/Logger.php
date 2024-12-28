1134838332040ecf748638d652e38f0e9fd2ddbf
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-12-29 02:05:40 +0700

    cs fix

--- src/Compat/Logger.php
+++ src/Compat/Logger.php
@@ -4,6 +4,21 @@ declare(strict_types=1);
 
 namespace ErrorHeroModule\Compat;
 
+use const E_COMPILE_ERROR;
+use const E_COMPILE_WARNING;
+use const E_CORE_ERROR;
+use const E_CORE_WARNING;
+use const E_DEPRECATED;
+use const E_ERROR;
+use const E_NOTICE;
+use const E_PARSE;
+use const E_RECOVERABLE_ERROR;
+use const E_USER_DEPRECATED;
+use const E_USER_ERROR;
+use const E_USER_NOTICE;
+use const E_USER_WARNING;
+use const E_WARNING;
+
 final class Logger extends \Laminas\Log\Logger
 {
     public static $errorPriorityMap = [
@@ -20,8 +35,8 @@ final class Logger extends \Laminas\Log\Logger
         E_COMPILE_ERROR     => self::ERR,
         E_COMPILE_WARNING   => self::ERR,
         // E_STRICT is deprecated in php 8.4
-        2048                => self::DEBUG,
-        E_DEPRECATED        => self::DEBUG,
-        E_USER_DEPRECATED   => self::DEBUG,
+        2048              => self::DEBUG,
+        E_DEPRECATED      => self::DEBUG,
+        E_USER_DEPRECATED => self::DEBUG,
     ];
 }
