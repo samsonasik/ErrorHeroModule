aee7f1aba97c1ffc57ff60919f40c8f92cdfa2c6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-12-29 03:02:55 +0700

    fix

--- src/Compat/LoggerAbstractServiceFactory.php
+++ src/Compat/LoggerAbstractServiceFactory.php
@@ -4,6 +4,20 @@ declare(strict_types=1);
 
 namespace ErrorHeroModule\Compat;
 
-class LoggerAbstractServiceFactory extends LoggerServiceFactory
+use Psr\Container\ContainerInterface;
+
+class LoggerAbstractServiceFactory extends \Laminas\Log\LoggerAbstractServiceFactory
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
+    {
+        $config = $this->getConfig($container);
+        $config = $config[$requestedName];
+
+        $this->processConfig($config, $container);
+
+        return new Logger($config);
+    }
 }
