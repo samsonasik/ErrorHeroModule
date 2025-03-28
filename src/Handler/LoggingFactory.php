984ded02a98d2a6e7ae58bc724af46a411174c1b
Henric Nylund <henric.nylund@mediaresearch.se>
2025-03-28 15:50:15 +0100

    Refactor logging configuration: remove Laminas DB, Mail & logger, add support for PSR Loggers

--- src/Handler/LoggingFactory.php
+++ src/Handler/LoggingFactory.php
@@ -4,61 +4,25 @@ declare(strict_types=1);
 
 namespace ErrorHeroModule\Handler;
 
-use ErrorHeroModule\Compat\Logger;
-use Laminas\Mail\Message;
-use Laminas\Mail\Transport\TransportInterface;
 use Psr\Container\ContainerInterface;
-use RuntimeException;
-
-use function sprintf;
+use Psr\Log\LoggerInterface;
 
 final class LoggingFactory
 {
-    /**
-     * @throws RuntimeException When mail config is enabled
-     * but mail-message and/or mail-transport config is not a service instance of Message.
-     */
     public function __invoke(ContainerInterface $container): Logging
     {
         /** @var array $config */
         $config = $container->get('config');
-        /** @var Logger $errorHeroModuleLogger */
+        /** @var LoggerInterface $errorHeroModuleLogger */
         $errorHeroModuleLogger = $container->get('ErrorHeroModuleLogger');
 
         $errorHeroModuleLocalConfig = $config['error-hero-module'];
-        $logWritersConfig           = $config['log']['ErrorHeroModuleLogger']['writers'];
-
-        $mailConfig           = $errorHeroModuleLocalConfig['email-notification-settings'];
-        $mailMessageService   = null;
-        $mailMessageTransport = null;
-
-        if ($mailConfig['enable'] === true) {
-            $mailMessageService = $container->get($mailConfig['mail-message']);
-            if (! $mailMessageService instanceof Message) {
-                throw new RuntimeException(sprintf(
-                    'You are enabling email log writer, your "mail-message" config must be instanceof %s',
-                    Message::class
-                ));
-            }
-
-            $mailMessageTransport = $container->get($mailConfig['mail-transport']);
-            if (! $mailMessageTransport instanceof TransportInterface) {
-                throw new RuntimeException(sprintf(
-                    'You are enabling email log writer, your "mail-transport" config must implements %s',
-                    TransportInterface::class
-                ));
-            }
-        }
-
-        $includeFilesToAttachments = $mailConfig['include-files-to-attachments'] ?? true;
+        $mailConfig                 = $errorHeroModuleLocalConfig['email-notification-settings'];
+        $includeFilesToAttachments  = $mailConfig['include-files-to-attachments'] ?? true;
 
         return new Logging(
             $errorHeroModuleLogger,
-            $errorHeroModuleLocalConfig,
-            $logWritersConfig,
-            $mailMessageService,
-            $mailMessageTransport,
-            $includeFilesToAttachments
+            $includeFilesToAttachments,
         );
     }
 }
