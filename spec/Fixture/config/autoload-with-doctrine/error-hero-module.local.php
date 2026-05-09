fcfca0765d1f9b8db5c17ed82ba883cd83548c9e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-05-09 22:16:45 +0700

    rectify

--- spec/Fixture/config/autoload-with-doctrine/error-hero-module.local.php
+++ spec/Fixture/config/autoload-with-doctrine/error-hero-module.local.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Pdo\Mysql;
 use Doctrine\DBAL\Driver\PDO\MySql\Driver;
 use Laminas\Db\Adapter\AdapterInterface;
--- spec/Fixture/config/module.local.php
+++ spec/Fixture/config/module.local.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ErrorHeroModule\Spec\Fixture\config;
 
 use ErrorHeroModule\Controller\ErrorPreviewController;
